eb42aead1a887c1b0e6f330b3025078be553d334
Paulo <vieira.dodt@gmail.com>
2018-03-22 17:15:00 -0300

    acrescentando o arquivo mongodb-delete que está incompleto

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,18 @@
+//const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connect to MongoDB server');
+    const db = client.db('TodoApp');
+    
+    //deleteMany
+
+    db.collection('Todos').deleteMany({text:'Buy a new car'}).then((result)=>{
+        console.log(result);
+    });
+
+
+});
