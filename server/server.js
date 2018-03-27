f9685e568abda640d4cc653f4228032c365d380c
Paulo <vieira.dodt@gmail.com>
2018-03-27 16:05:06 -0300

    add tests cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-const {ObjectID} = require('mongodb');
-const express = require('express');
-const bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
+var express = require('express');
+var bodyParser = require('body-parser');
 
 
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 
@@ -41,7 +41,7 @@ app.get('/todos/:id',(req,res)=>{
          if(!todo){
               return res.status(404).send();
           }
-          res.send(JSON.stringify(todo,undefined,2));   
+          res.send({todo});   
         }).catch((e)=>{
             res.status(400).send();
         })
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,16 +1,21 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
+
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [
     {
+        _id: new ObjectID,
         text:"First test Todo"
     },{
+        _id: new ObjectID,
         text:"Second test Todo"
     }
-]
+]; 
+
 beforeEach((done)=>{
     Todo.remove({}).then(()=>{
         return Todo.insertMany(todos);
@@ -71,3 +76,30 @@ describe('GET /todos',()=>{
             .end(done);
     })
 })
+
+describe('GET /todos/:id',()=>{
+    it('should return todo doc',(done)=>{
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+
+    it('should return 404 if todo not found',(done)=>{
+        var hexId = new ObjectID().toHexString();
+        request(app)
+            .get(`/todos/${hexId}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('shouuld return 404 for non-object ids',(done)=>{
+        request(app)
+            .get(`/todos/123abc`)
+            .expect(404)
+            .end(done);
+    });
+});
